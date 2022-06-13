df799dbd63e61195c701cadde8bbed7b47957e83
fsangar <57873286+fsangar@users.noreply.github.com>
2022-06-13 12:26:11 +0200

    Update settings.py

--- Server/teleasistencia/teleasistencia/settings.py
+++ Server/teleasistencia/teleasistencia/settings.py
@@ -68,21 +68,21 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = 'teleasistencia.asgi.application'
 
 # Para probar las alarmas sin necesidad del servidor Redis (sólo pruebas)
-#CHANNEL_LAYERS = {
-#    'default':{
-#        'BACKEND':'channels.layers.InMemoryChannelLayer'
-#    }
-#}
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 # En producción hay que usar un motor de almacenamiento Redis para alamacenar el Channel Layer
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("https://alumnos.iesvjp.es", 23844)],
-        },
-    },
-}
+#CHANNEL_LAYERS = {
+#    "default": {
+#        "BACKEND": "channels_redis.core.RedisChannelLayer",
+#        "CONFIG": {
+#            "hosts": [("https://alumnos.iesvjp.es", 23844)],
+#        },
+#    },
+#}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
